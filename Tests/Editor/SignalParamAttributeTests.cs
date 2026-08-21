2cc136ab92ecb747a87179fdabacb7dfa847ebb3
CNYT <cuneyt.taskent@gmail.com>
2026-08-21 11:38:06 +0300

    Test that an explicit zero index is not a bare attribute

--- Tests/Editor/SignalParamAttributeTests.cs
+++ Tests/Editor/SignalParamAttributeTests.cs
@@ -22,5 +22,17 @@ namespace FlowIoC.Tests
             Assert.That(attribute.HasIndex, Is.True);
             Assert.That(attribute.Index, Is.EqualTo(2));
         }
+
+        [Test]
+        public void An_explicit_zero_index_is_not_the_same_as_no_index()
+        {
+            var explicitZero = new SignalParamAttribute(0);
+            var bare = new SignalParamAttribute();
+
+            Assert.That(explicitZero.HasIndex, Is.True);
+            Assert.That(explicitZero.Index, Is.EqualTo(0));
+            Assert.That(bare.HasIndex, Is.False);
+            Assert.That(bare.Index, Is.EqualTo(0));
+        }
     }
 }
