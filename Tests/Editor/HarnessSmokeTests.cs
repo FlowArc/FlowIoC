1249c1cf17ace584fa21dfe73162591127b6085c
CNYT <cuneyt.taskent@gmail.com>
2026-08-21 11:28:26 +0300

    Assert the test assembly reaches FlowIoC internals

--- Tests/Editor/HarnessSmokeTests.cs
+++ Tests/Editor/HarnessSmokeTests.cs
@@ -1,14 +1,17 @@
-using FlowIoC.BaseModule.Injectable.Attributes;
+using FlowIoC.BaseModule.Controller.CommandGroup;
 using NUnit.Framework;
 
 namespace FlowIoC.Tests
 {
     public class HarnessSmokeTests
     {
+        // CommandGroupResolver is internal to the FlowIoC assembly, so this file only
+        // compiles when [InternalsVisibleTo("FlowIoC.Tests")] is in place. That is the
+        // point of the assertion: it proves the grant, not just the assembly reference.
         [Test]
-        public void The_test_assembly_can_see_the_FlowIoC_runtime_assembly()
+        public void The_test_assembly_can_see_the_FlowIoC_runtime_assembly_internals()
         {
-            Assert.That(typeof(SignalParamAttribute).Assembly.GetName().Name,
+            Assert.That(typeof(CommandGroupResolver).Assembly.GetName().Name,
                 Is.EqualTo("FlowIoC"));
         }
     }
